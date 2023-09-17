f7fb9366ed77dc905e39b394a2a72ad9c668329a
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-17 14:22:01 +0200

    Add basic frontend for polls
    Changes:
    - Added 'polls' page where the latest polls are displayed
    - Added 'polls/error404' page specifically for polls
    - Added 'polls/{pollId}' page where user can vote
    - Added 'UnexpectedError' extension for 'ISnackbar'

---
+++ PollsAppBlazor/Client/Extensions/SnackbarExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace PollsAppBlazor.Client.Extensions
+{
+	public static class SnackbarExtensions
+	{
+		public static void UnexpectedError(this ISnackbar snackbar)
+		{
+			snackbar.Add("An unexpected error has occurred. Please, try again.", Severity.Error,
+				options => options.CloseAfterNavigation = true);
+		}
+	}
+}
