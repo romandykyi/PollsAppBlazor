d71f49eb586dcec5f7e98ebe8193d93b4cb3f1e4
romandykyi <roman.dykyi.8210@gmail.com>
2023-09-18 17:29:45 +0200

    Add non-empty requirement for 'PollEditDto.Title'
    Fixes #6

--- PollsAppBlazor/Shared/Polls/PollEditDto.cs
+++ PollsAppBlazor/Shared/Polls/PollEditDto.cs
@@ -2,6 +2,7 @@
 {
 	public class PollEditDto
 	{
+		[MinLength(1, ErrorMessage = "Title cannot be empty")]
 		[StringLength(PollCreationDto.TitleMaxSize, ErrorMessage = "Title cannot exceed {1} characters")]
 		public string? Title { get; set; }
 		[StringLength(PollCreationDto.DescriptionMaxSize, ErrorMessage = "Description cannot exceed {1} characters")]
