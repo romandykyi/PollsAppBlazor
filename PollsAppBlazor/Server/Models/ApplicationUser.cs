0b3445695e2e8ea3519ff5db251f24fab8f2ed9a
romandykyi <roman.dykyi.8210@gmail.com>
2023-10-18 11:18:35 +0200

    Fix a typo

--- PollsAppBlazor/Server/Models/ApplicationUser.cs
+++ PollsAppBlazor/Server/Models/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace PollsAppBlazor.Server.Models
 	public class ApplicationUser : IdentityUser
 	{
 		/// <summary>
-		/// Polls which were createn by this user.
+		/// Polls which were created by this user.
 		/// </summary>
 		public ICollection<Poll>? CreatedPolls { get; set; }
 		/// <summary>
