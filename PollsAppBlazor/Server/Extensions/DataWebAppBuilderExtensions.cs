d9a9fc4f482b7cbdd2007fb2bce7ab30066f1c89
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-02 22:30:17 +0200

    Simplify Program.cs

---
+++ PollsAppBlazor/Server/Extensions/DataWebAppBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PollsAppBlazor.Server.DataAccess;
+
+namespace PollsAppBlazor.Server.Extensions;
+
+public static class DataWebAppBuilderExtensions
+{
+    public static WebApplicationBuilder ConnectDatabase(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return builder;
+    }
+}
---
+++ PollsAppBlazor/Server/Extensions/DebugWebAppBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace PollsAppBlazor.Server.Extensions;
+
+public static class DebugWebAppBuilderExtensions
+{
+    public static WebApplicationBuilder AddDebugOptions(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        return builder;
+    }
+}
