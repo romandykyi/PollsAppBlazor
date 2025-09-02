4062252831493e056fe83a120dfe9b17a9ba2238
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-03 01:07:50 +0200

    Add PollOption repository

---
+++ PollsAppBlazor.DataAccess/Repositories/Implementations/PollOptionRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PollsAppBlazor.DataAccess.Repositories.Interfaces;
+using PollsAppBlazor.Server.DataAccess;
+
+namespace PollsAppBlazor.DataAccess.Repositories.Implementations;
+
+public class PollOptionRepository(ApplicationDbContext dbContext) : IPollOptionRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    /// <summary>
+    /// Get ID of the Poll that contains the option.
+    /// </summary>
+    /// <param name="optionId">ID of the option</param>
+    /// <returns>
+    /// ID of the poll containing the option, or <see langword="null" /> if the option doesn't exist.
+    /// </returns>
+    public Task<int?> GetOptionPollIdAsync(int optionId)
+    {
+        return _dbContext.Options
+            .AsNoTracking()
+            .Where(o => o.Id == optionId)
+            .Select(o => (int?)o.PollId)
+            .FirstOrDefaultAsync();
+    }
+}
---
+++ PollsAppBlazor.DataAccess/Repositories/Interfaces/IPollOptionRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace PollsAppBlazor.DataAccess.Repositories.Interfaces;
+
+public interface IPollOptionRepository
+{
+    /// <summary>
+    /// Get ID of the Poll that contains the option.
+    /// </summary>
+    /// <param name="optionId">ID of the option</param>
+    /// <returns>
+    /// ID of the poll containing the option, or <see langword="null" /> if the option doesn't exist.
+    /// </returns>
+    Task<int?> GetOptionPollIdAsync(int optionId);
+}
