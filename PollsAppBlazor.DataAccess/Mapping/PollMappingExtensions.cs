583093d03f88cf950f4cdf8aa2583a3173be9bf5
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-03 21:37:02 +0200

    Add mapping extensions

---
+++ PollsAppBlazor.DataAccess/Mapping/PollMappingExtensions.cs
@@ -0,0 +1,57 @@
+﻿using PollsAppBlazor.Server.DataAccess.Models;
+using PollsAppBlazor.Shared.Polls;
+
+namespace PollsAppBlazor.DataAccess.Mapping;
+
+public static class PollMappingExtensions
+{
+    public static PollViewDto ToPollViewDto(this Poll poll, bool countVotes)
+    {
+        return new PollViewDto()
+        {
+            Id = poll.Id,
+            Title = poll.Title,
+            Description = poll.Description,
+            CreationDate = poll.CreationDate,
+            ExpiryDate = poll.ExpiryDate,
+            Creator = poll.Creator!.UserName!,
+            CreatorId = poll.CreatorId,
+            AreVotesVisible = poll.ResultsVisibleBeforeVoting,
+            // Select options
+            Options = poll.Options!.Select(o => new OptionViewDto()
+            {
+                Id = o.Id,
+                Description = o.Description,
+                VotesCount = countVotes ? o.Votes!.Count : null
+            }).ToList()
+        };
+    }
+
+    public static PollCreationDto ToPollCreationDto(this Poll poll)
+    {
+        return new PollCreationDto()
+        {
+            Description = poll.Description,
+            ExpiryDate = poll.ExpiryDate,
+            Options = poll.Options!.Select(o => new OptionCreationDto()
+            {
+                Description = o.Description
+            }).ToList(),
+            ResultsVisibleBeforeVoting = poll.ResultsVisibleBeforeVoting,
+            Title = poll.Title
+        };
+    }
+
+    public static PollPreviewDto ToPreviewDto(this Poll poll)
+    {
+        return new PollPreviewDto()
+        {
+            Id = poll.Id,
+            CreationDate = poll.CreationDate,
+            ExpiryDate = poll.ExpiryDate,
+            Creator = poll.Creator!.UserName!,
+            Title = poll.Title,
+            VotesCount = poll.Votes!.Count
+        };
+    }
+}
