db57132de4043efd7b142b1d4fabf27e4b02b75b
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-03 21:36:49 +0200

    Add the poll repository interface

--- PollsAppBlazor/Shared/Polls/PollViewDto.cs
+++ PollsAppBlazor/Shared/Polls/PollViewDto.cs
@@ -1,23 +1,24 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace PollsAppBlazor.Shared.Polls
+namespace PollsAppBlazor.Shared.Polls;
+
+public class PollViewDto
 {
-	public class PollViewDto
-	{
-		public int Id { get; set; }
+    public int Id { get; set; }
 
-		public string Title { get; set; } = null!;
-		public string? Description { get; set; }
-		public string Creator { get; set; } = null!;
-		public DateTimeOffset CreationDate { get; set; }
-		public DateTimeOffset? ExpiryDate { get; set; }
-		public IList<OptionViewDto> Options { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public string Creator { get; set; } = null!;
+    public DateTimeOffset CreationDate { get; set; }
+    public DateTimeOffset? ExpiryDate { get; set; }
+    public IList<OptionViewDto> Options { get; set; } = null!;
 
-		public bool CurrentUserCanEdit { get; set; }
-		public bool AreVotesVisible { get; set; }
-		public int? VotedOptionId { get; set; }
+    public bool CurrentUserCanEdit { get; set; }
+    public bool AreVotesVisible { get; set; }
+    public int? VotedOptionId { get; set; }
 
-		[JsonIgnore]
-		public bool IsExpired => DateTimeOffset.Now >= ExpiryDate;
-	}
+    [JsonIgnore]
+    public bool IsExpired => DateTimeOffset.Now >= ExpiryDate;
+    [JsonIgnore]
+    public string CreatorId { get; set; } = null!;
 }
