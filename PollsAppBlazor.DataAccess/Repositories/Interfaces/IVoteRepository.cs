a9ea18ba149a83df64cd56ca0b6dfd7c25dd0387
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-05 16:46:05 +0200

    Implement vote repository

---
+++ PollsAppBlazor.DataAccess/Repositories/Interfaces/IVoteRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace PollsAppBlazor.DataAccess.Repositories.Interfaces;
+
+public interface IVoteRepository
+{
+    /// <summary>
+    /// Gets an ID of an option which was voted by the user.
+    /// </summary>
+    /// <param name="pollId">ID of the poll.</param>
+    /// <param name="userId">ID of the user.</param>
+    /// <returns>
+    /// ID of the voted by the user option, or
+    /// <see langword="null" /> if user didn't vote on this poll.
+    /// </returns>
+    Task<int?> GetVotedOptionAsync(int pollId, string userId);
+
+    /// <summary>
+    /// Adds a vote for the option.
+    /// </summary>
+    /// <param name="pollId">ID of the poll which contains this option.</param>
+    /// <param name="optionId">ID of the option.</param>
+    /// <param name="userId">ID of the user who votes.</param>
+    Task VoteAsync(int pollId, int optionId, string userId);
+}
