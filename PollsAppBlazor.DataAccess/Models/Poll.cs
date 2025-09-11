d1903190778f33294f51199e413c668ffe133e3f
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-11 23:12:46 +0200

    Add Poll.IsDeleted

--- PollsAppBlazor.DataAccess/Models/Poll.cs
+++ PollsAppBlazor.DataAccess/Models/Poll.cs
@@ -2,46 +2,51 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PollsAppBlazor.Server.DataAccess.Models
+namespace PollsAppBlazor.Server.DataAccess.Models;
+
+public class Poll
 {
-	public class Poll
-	{
-		[Key]
-		public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-		[StringLength(PollCreationDto.TitleMaxSize)]
-		public string Title { get; set; } = null!;
-		[StringLength(PollCreationDto.DescriptionMaxSize)]
-		public string? Description { get; set; }
+    [StringLength(PollCreationDto.TitleMaxSize)]
+    public string Title { get; set; } = null!;
+    [StringLength(PollCreationDto.DescriptionMaxSize)]
+    public string? Description { get; set; }
 
-		public bool ResultsVisibleBeforeVoting { get; set; }
+    public bool ResultsVisibleBeforeVoting { get; set; }
 
-		public DateTimeOffset CreationDate { get; set; }
-		public DateTimeOffset? ExpiryDate { get; set; }
+    public bool IsDeleted { get; set; }
 
-		[NotMapped]
-		public bool IsActive => ExpiryDate == null || DateTimeOffset.Now < ExpiryDate;
+    public DateTimeOffset CreationDate { get; set; }
+    public DateTimeOffset? ExpiryDate { get; set; }
 
-		[ForeignKey(nameof(Creator))]
-		public string CreatorId { get; set; } = null!;
-		/// <summary>
-		/// User who created this poll.
-		/// </summary>
-		public ApplicationUser? Creator { get; set; }
+    /// <summary>
+    /// Returns <see langword="true" /> if the poll is still active (not expired, not deleted), 
+    /// otherwise <see langword="false" />. 
+    /// </summary>
+    [NotMapped]
+    public bool IsActive => !IsDeleted && (ExpiryDate == null || DateTimeOffset.Now < ExpiryDate);
 
-		/// <summary>
-		/// Options of this poll.
-		/// </summary>
-		public ICollection<Option>? Options { get; set; }
+    [ForeignKey(nameof(Creator))]
+    public string CreatorId { get; set; } = null!;
+    /// <summary>
+    /// User who created this poll.
+    /// </summary>
+    public ApplicationUser? Creator { get; set; }
 
-		/// <summary>
-		/// All votes related to this poll.
-		/// </summary>
-		public ICollection<Vote>? Votes { get; set; }
+    /// <summary>
+    /// Options of this poll.
+    /// </summary>
+    public ICollection<Option>? Options { get; set; }
 
-		/// <summary>
-		/// All favorites related to this poll.
-		/// </summary>
-		public ICollection<Favorite>? Favorites { get; set; }
-	}
+    /// <summary>
+    /// All votes related to this poll.
+    /// </summary>
+    public ICollection<Vote>? Votes { get; set; }
+
+    /// <summary>
+    /// All favorites related to this poll.
+    /// </summary>
+    public ICollection<Favorite>? Favorites { get; set; }
 }
