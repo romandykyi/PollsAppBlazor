4902da3d9df23e7b56bf8ea11e778022e9013c5a
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-09-18 23:55:03 +0200

    Add rate limit policies

---
+++ PollsAppBlazor/Server/Extensions/Safety/RateLimitServiceCollectionExtensions.cs
@@ -0,0 +1,100 @@
+﻿using IdentityModel;
+using Microsoft.AspNetCore.RateLimiting;
+using PollsAppBlazor.Server.Policy;
+using System.Threading.RateLimiting;
+
+namespace PollsAppBlazor.Server.Extensions.Safety;
+
+public static class RateLimitServiceCollectionExtensions
+{
+    private static RateLimiterOptions AddIPFixedWindowPolicy(this RateLimiterOptions options, string policyName, int permitLimit, TimeSpan window)
+    {
+        return options.AddPolicy(policyName, context =>
+        {
+            var clientIp = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+            return RateLimitPartition.GetFixedWindowLimiter(clientIp, _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = permitLimit,
+                Window = window,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+        });
+    }
+
+    private static RateLimiterOptions AddIPSlidingWindowPolicy(this RateLimiterOptions options, string policyName, int permitLimit, TimeSpan window)
+    {
+        return options.AddPolicy(policyName, context =>
+        {
+            var clientIp = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+            return RateLimitPartition.GetSlidingWindowLimiter(clientIp, _ => new SlidingWindowRateLimiterOptions
+            {
+                PermitLimit = permitLimit,
+                Window = window,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+        });
+    }
+
+    private static RateLimiterOptions AddUserFixedWindowPolicy(this RateLimiterOptions options, string policyName, int permitLimit, TimeSpan window)
+    {
+        return options.AddPolicy(policyName, context =>
+        {
+            string userId = context.User.FindFirst(JwtClaimTypes.Subject)?.Value ?? "unknown";
+            return RateLimitPartition.GetFixedWindowLimiter(userId, _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = permitLimit,
+                Window = window,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+        });
+    }
+
+    private static RateLimiterOptions AddUserSlidingWindowPolicy(this RateLimiterOptions options, string policyName, int permitLimit, TimeSpan window)
+    {
+        return options.AddPolicy(policyName, context =>
+        {
+            string userId = context.User.FindFirst(JwtClaimTypes.Subject)?.Value ?? "unknown";
+            return RateLimitPartition.GetSlidingWindowLimiter(userId, _ => new SlidingWindowRateLimiterOptions
+            {
+                PermitLimit = permitLimit,
+                Window = window,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+        });
+    }
+
+    public static IServiceCollection AddRateLimiting(this IServiceCollection services)
+    {
+        services.AddRateLimiter(options =>
+        {
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+            {
+                var clientIp = httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+                return RateLimitPartition.GetFixedWindowLimiter(clientIp, _ => new FixedWindowRateLimiterOptions
+                {
+                    PermitLimit = 100,
+                    Window = TimeSpan.FromMinutes(1),
+                    QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                    QueueLimit = 0
+                });
+            });
+            options.RejectionStatusCode = 429; // Too Many Requests
+
+            options
+                .AddIPFixedWindowPolicy(RateLimitingPolicy.LogInShortPolicy, 15, TimeSpan.FromMinutes(1))
+                .AddIPSlidingWindowPolicy(RateLimitingPolicy.LogInLongPolicy, 100, TimeSpan.FromDays(1))
+                .AddIPFixedWindowPolicy(RateLimitingPolicy.RegisterShortPolicy, 15, TimeSpan.FromMinutes(1))
+                .AddIPSlidingWindowPolicy(RateLimitingPolicy.RegisterLongPolicy, 100, TimeSpan.FromDays(1))
+                .AddUserSlidingWindowPolicy(RateLimitingPolicy.CreateShortPolicy, 10, TimeSpan.FromMinutes(1))
+                .AddUserSlidingWindowPolicy(RateLimitingPolicy.CreateLongPolicy, 25, TimeSpan.FromDays(1))
+                .AddUserFixedWindowPolicy(RateLimitingPolicy.EditShortPolicy, 15, TimeSpan.FromMinutes(1))
+                .AddUserSlidingWindowPolicy(RateLimitingPolicy.EditLongPolicy, 500, TimeSpan.FromDays(1))
+                .AddUserSlidingWindowPolicy(RateLimitingPolicy.VotePolicy, 1000, TimeSpan.FromDays(1));
+        });
+        return services;
+    }
+}
---
+++ PollsAppBlazor/Server/Policy/RateLimitingPolicy.cs
@@ -0,0 +1,18 @@
+﻿namespace PollsAppBlazor.Server.Policy;
+
+public static class RateLimitingPolicy
+{
+    public const string LogInShortPolicy = "LogInShortPolicy";
+    public const string LogInLongPolicy = "LogInLongPolicy";
+
+    public const string RegisterShortPolicy = "RegisterShortPolicy";
+    public const string RegisterLongPolicy = "RegisterLongPolicy";
+
+    public const string CreateShortPolicy = "CreateShortPolicy";
+    public const string CreateLongPolicy = "CreateLongPolicy";
+
+    public const string EditShortPolicy = "EditShortPolicy";
+    public const string EditLongPolicy = "EditLongPolicy";
+
+    public const string VotePolicy = "VotePolicy";
+}
