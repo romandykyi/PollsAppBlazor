4b1e940d407f9d9055fd2413bb3f64ebd9714be0
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-11-09 13:35:17 +0100

    Implement refresh token cookie service

---
+++ PollsAppBlazor.Application/Services/Auth/Http/IRefreshTokenCookieService.cs
@@ -0,0 +1,10 @@
+﻿namespace PollsAppBlazor.Application.Services.Auth.Http;
+
+public interface IRefreshTokenCookieService
+{
+    string? GetRefreshTokenFromCookie();
+
+    void SetRefreshTokenCookie(string refreshToken, TimeSpan? maxAge);
+
+    void DeleteRefreshTokenCookie();
+}
